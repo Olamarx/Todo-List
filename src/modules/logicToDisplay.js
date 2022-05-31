c4a403cec0d8def6b27dcf01c1c72f3190e6840c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-05-31 09:55:10 +0100

    Add the input function

--- src/modules/logicToDisplay.js
+++ src/modules/logicToDisplay.js
@@ -1,3 +1,4 @@
+import Todo from './todoClass.js';
 import UI from './UI.js';
 
 export default class Logic {
@@ -5,5 +6,10 @@ export default class Logic {
         window.addEventListener('load', UI.loadLocalStoreAndDisplayHTML())
     }
 
-    
+    static addFunc = () => {
+        const input = document.querySelector('#input')
+        const task = input.value;
+        const index = 
+        const todoClass = new Todo()
+    }
 }
