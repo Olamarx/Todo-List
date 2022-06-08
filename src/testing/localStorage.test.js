3e07c821e22dc1e28f552e40ea5206da9c874463
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-08 15:04:51 -0500

    Test pass

--- src/testing/localStorage.test.js
+++ src/testing/localStorage.test.js
@@ -18,4 +18,28 @@ describe('Test for add and delete functions', () => {
     expect(push).not.toBe(library);
   });
 
+  test('Test for delete function', () => {
+    // Arrange
+    let library;
+    if (!library) {
+      library = [];
+    } else {
+      library = JSON.parse(localStorage.getItem('library'));
+    }
+    const content = {
+      completed: false,
+      task: 'words',
+      index: 1,
+    };
+      // Act
+    library.push(content);
+    library.forEach((todo) => {
+      if (`${todo.completed}`.trim() === false) {
+        library.splice(library.indexOf(todo), 1);
+      }
+    });
+    // Assert
+    expect(library).toBe(library);
+  });
+
 });
