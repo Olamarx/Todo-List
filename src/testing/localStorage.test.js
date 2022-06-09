3515e10c679e91533c49f026ad210a95bf4f74cf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-06-09 22:50:18 +0100

    Add the test for edit function and the function for update function

--- src/testing/localStorage.test.js
+++ src/testing/localStorage.test.js
@@ -1,4 +1,4 @@
-describe('Test for add and delete functions', () => {
+describe('Test for add, edit, completed, delete, clear all completed functions', () => {
   test('Test for add function', () => {
     // Arrange
     let library;
@@ -40,5 +40,56 @@ describe('Test for add and delete functions', () => {
     });
     // Assert
     expect(library).toBe(library);
+  });
+
+  test('Test for edit function', () => {
+    // Arrange
+    let library;
+    if (!library) {
+      library = [];
+    } else {
+      library = JSON.parse(localStorage.getItem('library'));
+    }
+    const content = {
+      completed: true,
+      task: 'words',
+      index: 1,
+    };
+      // Act
+    library.push(content);
+    library.forEach((todo) => {
+      if (todo.task === content.task) {
+        todo.completed = !todo.completed;
+      }
+    });
+    // Assert
+    expect(library[0].completed).toBeFalsy();
+    expect(library[0].completed).toBe(false);
+  });
+
+  test('Test for completed function', () => {
+    // Arrange
+    let library;
+    if (!library) {
+      library = [];
+    } else {
+      library = JSON.parse(localStorage.getItem('library'));
+    }
+    const content = {
+      completed: false,
+      task: 'words',
+      index: 1,
+    };
+    const newTodo = 'This is a new task';
+    // Act
+    library.push(content);
+    library.forEach((todo) => {
+      if (todo.task === content.task) {
+        todo.task = newTodo;
+      }
+    });
+    // Assert
+    expect(library[0].task).toBe('This is a new task');
+    expect(library[0].task).not.toBe('words');
   });
 });
