d8429b83329e513a8c9476f7c0e7c84b33120a62
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-06-09 16:58:08 -0500

    Add test for clear all completed function

--- src/testing/localStorage.test.js
+++ src/testing/localStorage.test.js
@@ -92,4 +92,54 @@ describe('Test for add, edit, completed, delete, clear all completed functions',
     expect(library[0].task).toBe('This is a new task');
     expect(library[0].task).not.toBe('words');
   });
+  test('Test for clear all completed function', () => {
+    // Arrange
+    let library;
+    if (!library) {
+      library = [];
+    } else {
+      library = JSON.parse(localStorage.getItem('library'));
+    }
+    const content1 = {
+      completed: true,
+      task: 'words',
+      index: 1,
+    };
+    const content2 = {
+      completed: false,
+      task: 'words',
+      index: 2,
+    };
+    const content3 = {
+      completed: false,
+      task: 'words',
+      index: 3,
+    };
+    const content4 = {
+      completed: true,
+      task: 'words',
+      index: 4,
+    };
+    const content5 = {
+      completed: true,
+      task: 'words',
+      index: 5,
+    };
+    // Act
+    library.push(content1);
+    library.push(content2);
+    library.push(content3);
+    library.push(content4);
+    library.push(content5);
+
+    const filt = library.filter((todo) => todo.completed === true)
+    // Assert
+    expect(filt[0].completed).toBe(true);
+    expect(filt[0].completed).not.toBeFalsy();
+    expect(filt[1].completed).toBe(true);
+    expect(filt[1].completed).not.toBeFalsy();
+    expect(filt[2].completed).toBe(true);
+    expect(filt[2].completed).not.toBeFalsy();
+    });
+
 });
